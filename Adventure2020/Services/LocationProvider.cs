5febb063f7e640c6530d0d814d56d8693fb4847a
Filip Plass <filip.plass@pslib.cz>
2020-04-23 17:27:44 +0200

    invent upgrade metod atd plus mistnosti

--- Adventure2020/Services/LocationProvider.cs
+++ Adventure2020/Services/LocationProvider.cs
@@ -13,6 +13,7 @@ namespace Adventure2020.Services
 
         public LocationProvider()
         {
+            GameState gs = new GameState();
             _locations = new Dictionary<int, ILocation>();
             _map = new List<Connection>();
             _locations.Add(0, new Location { Description = "This is where our story starts." }); // Game starts
@@ -22,11 +23,13 @@ namespace Adventure2020.Services
             _locations.Add(4, new Location {Description = "You are standing on the GALLERY..."});
             _locations.Add(5, new Location {Description = "You are looking out of the window. The window is locked ..."});
             _locations.Add(6, new Location {Description = "There is just a regular bookshelf. Oh wait, what does this little metal button?"});
+            _locations.Add(7, new Location {Description = "" });
             _map.Add(new Connection(0, 2, "Go to hall"));
             _map.Add(new Connection(2, 3, "Visit Library", (gs) => { if (gs.HP > 10) return true; return false; }));
             _map.Add(new Connection(3,4, "Walk up the little stairs in the library to the gallery"));
             _map.Add(new Connection(4,5,"Go left to look closely at the window")); // s oknem pak půjde něco udělat
             _map.Add(new Connection(4,6, "Go to the right to see the end of the gallery"));
+            //_map.Add(new Connection(6,7, "Push the button and go to the secret room", (gs) =>{if(gs.IsItemInHand("item") == true)}
         }
 
         public bool ExistsLocation(int id)
