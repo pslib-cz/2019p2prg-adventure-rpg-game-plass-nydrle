dc76c66d2cb5451f49a5ffc023432865d7efe3d6
Filip Plass <filip.plass@pslib.cz>
2020-05-21 09:48:58 +0200

    h

--- Adventure2020/Startup.cs
+++ Adventure2020/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -23,10 +24,7 @@ namespace Adventure2020
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDistributedMemoryCache();
-
-            services.AddSession();
-            
+         
             services.AddRazorPages();
         }
 
